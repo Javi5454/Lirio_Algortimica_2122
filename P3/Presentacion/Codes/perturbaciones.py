84c446ddf1f76781ed846d7c2f2e33bb86bd1536
Javi5454 <javier-gomezlopez@outlook.es>
2022-05-16 23:37:19 +0200

    First and third heuristic at presentation P3

--- P3/Presentacion/Codes/perturbaciones.py
+++ P3/Presentacion/Codes/perturbaciones.py
@@ -1,23 +1,9 @@
-#Find the most separated nodes (only one of them)
-def get_worst_node(road):
-    worst_distance = distance(road[0], road[1])
-    worst_pos = 0
-
-    for i in range(len(road)-1):
-        if distance(road[i],road[i+1]) > worst_distance:
-            worst_distance = distance(road[i],road[i+1])
-            worst_pos = i
-
-    return worst_pos
-
-#Perturbate the road from a pos
 def perturbate(road, orden, pos):
     current_perb = road
     best_gain = math.inf
     best_perturbation = pos
     base_distance = get_road_distance(road)
 
-    #Calcualte all perturbations
     for i in range(len(road)):
         current_perb = get_swap(road, pos, i)
         swap_distance = get_road_distance(current_perb)
@@ -26,11 +12,8 @@ def perturbate(road, orden, pos):
             best_perturbation = i
             base_distance = swap_distance
 
-    #Make the change with the best perturbation
     road = get_swap(road, pos, best_perturbation)
     orden = get_swap(orden, pos, best_perturbation)
-
-
 
 def get_best_solution_perturbations(points, orden, perturbations):
     base_road = points
